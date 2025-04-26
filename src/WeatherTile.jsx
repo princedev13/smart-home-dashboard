d770bae101d6095e315304f0768b913aefef2f9d
princedev13 <princefoster73@gmail.com>
2025-04-26 03:08:22 -0400

    added weathertile and index

---
+++ src/WeatherTile.jsx
@@ -0,0 +1,17 @@
+function WeatherTile () {
+
+    return (
+
+        <>
+        
+            <div class="weather-tile">
+                <h2>Temperature</h2>
+            </div>
+        
+        </>
+
+    );
+
+}
+
+export default WeatherTile
