be0870ac71a4d3d04dd6c4dd51224767e97435d4
princedev13 <princefoster73@gmail.com>
2025-04-27 03:31:09 -0400

    added weather app get request

--- src/WeatherTile.jsx
+++ src/WeatherTile.jsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from 'react';
+const WEATHER_API_KEY = import.meta.env.WEATHER_API_SECRET;
+
 function WeatherTile () {
+    const [data, setData] = useState(null);
+
+    function getWeather () {
+        useEffect(() => {
+        fetch('http://api.weatherapi.com/v1/current.json?key=WEATHER_API_KEY&q=Orlando')
+            .then(data => {
+                console.log(data)
+            })
+            .catch(error => {
+                console.error('error fetching data', error);
+            });
+        })
+
+    }
 
     return (
 
@@ -6,6 +23,8 @@ function WeatherTile () {
         
             <div class="weather-tile">
                 <h2>Temperature</h2>
+                <h2>icon</h2>
+                <h4>temp</h4>
             </div>
         
         </>
